168225c084c3f3a07a69acdea780b6e41a5e6f88
franek86 <frane.konjevoda@gmail.com>
2024-01-16 22:37:20 +0100

    working on upload image file and post form data in the backand api

--- app/(admin)/manager/countries/create/page.jsx
+++ app/(admin)/manager/countries/create/page.jsx
@@ -1,7 +1,20 @@
+"use client";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { ArrowUpTrayIcon } from "@heroicons/react/24/outline";
+
 import Breadcrumbs from "@/app/components/Breadcrumbs";
+import FormInput from "@/app/components/FormInput.jsx";
+import FormUpload from "@/app/components/FormUpload.jsx";
+import { Button } from "@/app/components/Button.jsx";
 
 const CreateCountry = () => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
       <Breadcrumbs
@@ -11,6 +24,17 @@ const CreateCountry = () => {
           { label: "Create", href: "/manager/countries/create", active: true },
         ]}
       />
+
+      <form className='mt-6' onSubmit={handleSubmit(onSubmit)}>
+        <FormInput register={register("name")} id='country' type='text' label='Country name' name='name' placeholder='Please enter country name' />
+        <FormInput register={register("shortCountryCode")} id='shortCountryCode' type='text' name='shortCountryCode' label='Short code' placeholder='Please enter short code e.g EN' />
+        <FormInput register={register("longCountryCode")} id='longCountryCode' type='text' name='longCountryCode' label='Long code' placeholder='Please enter long code e.g ENG' />
+        <FormUpload register={register("iconFlag")} icon={<ArrowUpTrayIcon className='h-8 w-8' />} />
+
+        <Button type='submit' classVariation='btn btn--primary'>
+          Create
+        </Button>
+      </form>
     </>
   );
 };
